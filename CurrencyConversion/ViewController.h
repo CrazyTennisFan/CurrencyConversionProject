5f81e305e8811e28c85ff1c24d70aa3a93f876e8
Lucas Yang <lucas@fionas-macbook-pro.local>
2017-05-31 19:28:48 -0400

    Initial Commit

---
+++ CurrencyConversion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConversion
+//
+//  Created by Lucas Yang on 2017-05-31.
+//  Copyright © 2017 Lucas Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CurrencyConversion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CurrencyConversion
+//
+//  Created by Lucas Yang on 2017-05-31.
+//  Copyright © 2017 Lucas Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
